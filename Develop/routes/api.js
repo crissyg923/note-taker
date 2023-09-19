7202dc7e7539c5f17dff955bd97cdf3522c42735
Crystal Green <crissybrown1992@icloud.com>
2023-09-19 19:56:11 -0400

    replaced nanoid with uuid because of es6 syntax errors and created successful get request for notes.json

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,8 +1,40 @@
 const api=require('express').Router();
-const{nanoid}=require('nanoid');
+// import {nanoid} from 'nanoid';
+const { v4: uuidv4 } = require('uuid');
 const fs=require('fs');
 
-api.get('/', (req,res) => {
+api.get('/notes', (req,res) => {
     fs.readFile('./db/notes.json', 'utf-8', (error,data) =>
     error ? console.error(error) : res.json(JSON.parse(data)));
 });
+
+api.post('/notes', (req,res) => {
+    console.log(req.body);
+    const { noteTitle, noteText} = req.body;
+
+    if (req.body) {
+        const newNote = {
+            noteTitle,
+            noteText,
+            note_id: uuidv4(),
+        };
+       fs.readFile('./db/notes.json', 'utf-8', (error, data) => {
+            if(error) {
+                console.error('Error in adding note');
+            } else {
+       const noteData=JSON.parse(data);
+       noteData.push(newNote);
+       fs.writeFile('./db/notes.html', noteData, (err) => {
+       if (err) {
+         console.error(err);
+       } else {
+         console.log('Successfully added note!');
+       } 
+    });
+}
+});
+}
+});
+    
+
+module.exports = api;
